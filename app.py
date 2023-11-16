ad8f8b5a424969909468fa1fe3afcf1e9d23453d
Nicholasdahlasb <nasbjornsen@elev.rogfk.no>
2023-11-16 12:49:40 +0100

    ny
    ._.

--- app.py
+++ app.py
@@ -10,5 +10,13 @@ def home():
 def server():
     return render_template('server.html')
 
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True,host="0.0.0.0")
